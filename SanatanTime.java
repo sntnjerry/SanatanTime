afeca3303a92e282104ccaf13063f29879490310
sntnjerry <75675433+sntnjerry@users.noreply.github.com>
2020-12-11 18:49:59 +0530

    Add files via upload

--- SanatanTime.java
+++ SanatanTime.java
@@ -26,6 +26,7 @@ Jai Shree Ram!
 
 */
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class SanatanTime
@@ -46,23 +47,29 @@ public class SanatanTime
         }
         this.sanatan_time_start = sanatan_time_start;
         Date christian_current_time = new Date();
-        int[] sanatan_converted_time = christian_to_sanatan_time(christian_current_time.getHours(), christian_current_time.getMinutes(), christian_current_time.getSeconds());
+        SimpleDateFormat formatter = new SimpleDateFormat("SSSS");
+        int milliseconds = Integer.parseInt(formatter.format(christian_current_time));
+        double seconds = (double)(christian_current_time.getSeconds()) + (double)((double)(milliseconds) / 1000);
+        int[] sanatan_converted_time = christian_to_sanatan_time(christian_current_time.getHours(), christian_current_time.getMinutes(), seconds);
         this.sanatan_ghadis = sanatan_converted_time[0];
         this.sanatan_pals = sanatan_converted_time[1];
         this.sanatan_lipts = sanatan_converted_time[2];
         this.sanatan_vilipts = sanatan_converted_time[3];
-        this.current_sanatan_time = this.sanatan_ghadis + " Ghadi, " + this.sanatan_pals + " Pal, " + this.sanatan_lipts + " Lipt, " + this.sanatan_vilipts + " Vilipt";
+        this.current_sanatan_time = this.sanatan_ghadis + " Ghadis, " + this.sanatan_pals + " Pals, " + this.sanatan_lipts + " Lipts, " + this.sanatan_vilipts + " Vilipts";
     }
     
     public SanatanTime()
     {
         Date christian_current_time = new Date();
-        int[] sanatan_converted_time = christian_to_sanatan_time(christian_current_time.getHours(), christian_current_time.getMinutes(), christian_current_time.getSeconds());
+        SimpleDateFormat formatter = new SimpleDateFormat("SSSS");
+        int milliseconds = Integer.parseInt(formatter.format(christian_current_time));
+        double seconds = (double)(christian_current_time.getSeconds()) + (double)((double)(milliseconds) / 1000);
+        int[] sanatan_converted_time = christian_to_sanatan_time(christian_current_time.getHours(), christian_current_time.getMinutes(), seconds);
         this.sanatan_ghadis = sanatan_converted_time[0];
         this.sanatan_pals = sanatan_converted_time[1];
         this.sanatan_lipts = sanatan_converted_time[2];
         this.sanatan_vilipts = sanatan_converted_time[3];
-        this.current_sanatan_time = this.sanatan_ghadis + " Ghadi, " + this.sanatan_pals + " Pal, " + this.sanatan_lipts + " Lipt, " + this.sanatan_vilipts + " Vilipt";
+        this.current_sanatan_time = this.sanatan_ghadis + " Ghadis, " + this.sanatan_pals + " Pals, " + this.sanatan_lipts + " Lipts, " + this.sanatan_vilipts + " Vilipts";
     }
     
     private int hours_difference(int start_hour, int end_hour)
@@ -77,7 +84,7 @@ public class SanatanTime
         }
     }
     
-    private int[] christian_to_sanatan_time(int hours, int minutes, int seconds)
+    private int[] christian_to_sanatan_time(int hours, int minutes, double seconds)
     {
         int minutes_from_day_start;
         if (hours == this.sanatan_time_start[0] && minutes >= this.sanatan_time_start[1])
@@ -90,16 +97,19 @@ public class SanatanTime
         }
         int ghadis = (int)((double)(minutes_from_day_start / 24));
         int minutes_from_ghadi_start = minutes_from_day_start % 24;
-        int pals = (int)((double)(((minutes_from_ghadi_start * 60) + seconds) / 24));
-        int lipts = (int)((double)((((minutes_from_ghadi_start * 60) + seconds) % 24) / 0.4));
-        int vilipts = (int)((double)((double)((((minutes_from_ghadi_start * 60) + seconds) % 24) % 0.4) * 150));
+        int pals = (int)((double)((double)((minutes_from_ghadi_start * 60) + seconds) / 24));
+        int lipts = (int)((double)(((double)((minutes_from_ghadi_start * 60) + seconds) % 24) / 0.4));
+        int vilipts = (int)((double)((double)(((double)((minutes_from_ghadi_start * 60) + seconds) % 24) % 0.4) * 150));
         return new int[]{ghadis, pals, lipts, vilipts};
     }
     
     public String convert(Date christian_time)
     {
-        int[] sanatan_converted_time = christian_to_sanatan_time(christian_time.getHours(), christian_time.getMinutes(), christian_time.getSeconds());
-        return sanatan_converted_time[0] + " Ghadi, " + sanatan_converted_time[1] + " Pal, " + sanatan_converted_time[2] + " Lipt, " + sanatan_converted_time[3] + " Vilipt";
+        SimpleDateFormat formatter = new SimpleDateFormat("SSSS");
+        int milliseconds = Integer.parseInt(formatter.format(christian_time));
+        double seconds = (double)(christian_time.getSeconds()) + (double)((double)(milliseconds) / 1000);
+        int[] sanatan_converted_time = christian_to_sanatan_time(christian_time.getHours(), christian_time.getMinutes(), seconds);
+        return sanatan_converted_time[0] + " Ghadis, " + sanatan_converted_time[1] + " Pals, " + sanatan_converted_time[2] + " Lipts, " + sanatan_converted_time[3] + " Vilipts";
     }
     
     @Override
